786f90ae6af1a959eb3eed0f31163435f03af3fe
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-02-03 23:29:00 -0300

    refactor(model):implemented lombok annotations to the Employee class

--- src/main/java/com/mlcdev/employeeapi/model/Employee.java
+++ src/main/java/com/mlcdev/employeeapi/model/Employee.java
@@ -1,18 +1,25 @@
 package com.mlcdev.employeeapi.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Objects;
+
 
 @Entity
 @Table(name = "tb_employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
@@ -24,78 +31,4 @@ public class Employee {
     @Column(name = "role", nullable = false)
     private Role role;
 
-    public Employee() {
-    }
-
-    public Employee(String name, BigDecimal salary, LocalDate hiringDate, Role role) {
-        this.name = name;
-        this.salary = salary;
-        this.hiringDate = hiringDate;
-        this.role = role;
-    }
-
-    public Employee(Long id, String name, BigDecimal salary, LocalDate hiringDate, Role role) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-        this.hiringDate = hiringDate;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getSalary() {
-        return salary;
-    }
-
-    public void setSalary(BigDecimal salary) {
-        this.salary = salary;
-    }
-
-    public LocalDate getHiringDate() {
-        return hiringDate;
-    }
-
-    public void setHiringDate(LocalDate hiringDate) {
-        this.hiringDate = hiringDate;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Objects.equals(id, employee.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return getId() + " | " + getName() + " | " + String.format("%.2f", getSalary()) + " | " + getRole() + " | Hired At: " + getHiringDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-    }
 }
