08720e5677597eb9b0bfb763ee583d880fe6b063
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-02-04 20:38:41 -0300

    feat(controller): implement create method in EmployeeController

--- src/main/java/com/mlcdev/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/mlcdev/employeeapi/controller/EmployeeController.java
@@ -1,13 +1,34 @@
 package com.mlcdev.employeeapi.controller;
 
+import com.mlcdev.employeeapi.dto.EmployeeDTO;
+import com.mlcdev.employeeapi.service.EmployeeService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/employee")
 public class EmployeeController {
 
+    private final EmployeeService service;
 
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> create(@Valid @RequestBody EmployeeDTO dto) {
+        EmployeeDTO savedDto = service.add(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedDto.getId()).toUri();
+        log.debug("Returning the response containing the crated employee DTO");
+        return ResponseEntity.created(uri).body(savedDto);
+    }
 }
