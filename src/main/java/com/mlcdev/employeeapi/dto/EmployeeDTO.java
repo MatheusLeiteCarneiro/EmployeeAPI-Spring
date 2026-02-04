4691368d5ebfcf0324aa1d7eb75ca5d2617c895e
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-02-03 23:17:42 -0300

    feat(dto): add validation annotations to EmployeeDTO fields

--- src/main/java/com/mlcdev/employeeapi/dto/EmployeeDTO.java
+++ src/main/java/com/mlcdev/employeeapi/dto/EmployeeDTO.java
@@ -1,6 +1,10 @@
 package com.mlcdev.employeeapi.dto;
 
 import com.mlcdev.employeeapi.model.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +19,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class EmployeeDTO {
     private Long id;
+
+    @NotNull(message = "The name can't be null")
+    @NotBlank(message = "Name can't be blank")
     private String name;
+
+    @NotNull(message = "The salary can't be null")
+    @Positive(message = "The salary must be positive")
     private BigDecimal salary;
+
+    @NotNull(message = "The hiring date can't be null")
     private LocalDate hiringDate;
+
+    @NotNull(message = "You must specify the employee role")
     private Role role;
 
 }
