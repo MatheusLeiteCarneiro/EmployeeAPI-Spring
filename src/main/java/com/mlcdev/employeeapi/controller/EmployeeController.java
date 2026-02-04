68aed47cafcb65a490394fc5b98296fd70b00a7f
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-02-04 20:33:24 -0300

    feat(controller): add EmployeeController to handle requests

---
+++ src/main/java/com/mlcdev/employeeapi/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.mlcdev.employeeapi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/employee")
+public class EmployeeController {
+
+
+}
