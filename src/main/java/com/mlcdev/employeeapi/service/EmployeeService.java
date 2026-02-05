96782cbbc63eecf224172a65bdc711fd79461489
MatheusCarneiro <matheus.leite.carneiro@gmail.com>
2026-02-04 21:22:04 -0300

    feat(service): implement findAll method in EmployeeService

--- src/main/java/com/mlcdev/employeeapi/service/EmployeeService.java
+++ src/main/java/com/mlcdev/employeeapi/service/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.mlcdev.employeeapi.service;
 
 import com.mlcdev.employeeapi.dto.EmployeeDTO;
+import com.mlcdev.employeeapi.exception.NotFoundException;
 import com.mlcdev.employeeapi.mapper.EmployeeMapper;
 import com.mlcdev.employeeapi.model.Employee;
 import com.mlcdev.employeeapi.repository.EmployeeRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +19,13 @@ public class EmployeeService {
 
     public EmployeeService(EmployeeRepository repository) {
         this.repository = repository;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EmployeeDTO> findAll(Pageable pageable){
+        Page<Employee> result = repository.findAll(pageable);
+        log.info("Page found with {} elements out of a total of: {}",result.getNumberOfElements(),result.getTotalElements());
+        return result.map(EmployeeMapper::toDTO);
     }
 
     @Transactional
